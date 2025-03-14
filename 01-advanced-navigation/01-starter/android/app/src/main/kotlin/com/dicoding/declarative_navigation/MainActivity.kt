68d58d1c964229deaf07d59c219ed6a9af4854cb
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2025-03-14 08:27:04 +0700

    update advanced navigation native project

---
+++ 01-advanced-navigation/01-starter/android/app/src/main/kotlin/com/dicoding/declarative_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dicoding.declarative_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- 01-advanced-navigation/01-starter/android/app/src/main/kotlin/com/example/declarative_navigation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.declarative_navigation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
