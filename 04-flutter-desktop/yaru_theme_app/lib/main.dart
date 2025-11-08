bae7c93f45874a5ed1a1c6b6962f4020bb45ee28
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2025-11-08 07:54:48 +0700

    update yaru design app

--- 04-flutter-desktop/yaru_theme_app/lib/main.dart
+++ 04-flutter-desktop/yaru_theme_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
-import 'package:yaru_icons/yaru_icons.dart';
-import 'package:yaru_widgets/yaru_widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,28 +28,28 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return YaruPage(
-      children: [
-        const Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Outlined Button"),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text("Text Button"),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Elevated Button"),
-            ),
-          ],
-        ),
-      ],
+    return YaruDetailPage(
+      body: Column(
+        children: [
+          const Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text("Outlined Button"),
+              ),
+              TextButton(onPressed: () {}, child: const Text("Text Button")),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Elevated Button"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -64,10 +62,10 @@ class SecondScreen extends StatelessWidget {
     return ListView.builder(
       itemCount: 20,
       itemBuilder: (context, index) {
-        return YaruRow(
-          actionWidget: Icon(YaruIcons.information),
-          trailingWidget: Text("Item ${index + 1}"),
-          description: "Subtitle ${index + 1}",
+        return YaruTile(
+          trailing: const Icon(YaruIcons.information),
+          title: Text("Item ${index + 1}"),
+          subtitle: Text("Subtitle ${index + 1}"),
           enabled: true,
         );
       },
@@ -81,22 +79,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return YaruMasterDetailPage(
-      pageItems: [
-        YaruPageItem(
-          titleBuilder: (context) => Text("First Screen"),
-          builder: (context) => FirstScreen(),
-          iconData: YaruIcons.document,
-        ),
-        YaruPageItem(
-          titleBuilder: (context) => Text("Second Screen"),
-          builder: (context) => SecondScreen(),
-          iconData: YaruIcons.format_ordered_list,
-        ),
-      ],
-      leftPaneWidth: 280,
-      appBar: AppBar(
-        title: Text("Yaru Theme App"),
-      ),
+      length: 2,
+      initialIndex: 0,
+      tileBuilder: (context, index, selected, availableWidth) {
+        if (index == 0) {
+          return const YaruMasterTile(
+            leading: Icon(YaruIcons.document),
+            title: Text("First Screen"),
+          );
+        } else {
+          return const YaruMasterTile(
+            leading: Icon(YaruIcons.ordered_list),
+            title: Text("Second Screen"),
+          );
+        }
+      },
+      pageBuilder: (context, index) {
+        if (index == 0) {
+          return const FirstScreen();
+        } else {
+          return const SecondScreen();
+        }
+      },
+      appBar: AppBar(title: const Text("Yaru Theme App")),
     );
   }
 }
